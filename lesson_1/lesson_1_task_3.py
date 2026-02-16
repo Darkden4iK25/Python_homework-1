665e53391357580f11c2fc1d1eb89afc38144d80
Денис <deeeniiis01@gmail.com>
2026-02-16 17:48:02 +1100

    Update lesson_1_task_3.py

--- lesson_1/lesson_1_task_3.py
+++ lesson_1/lesson_1_task_3.py
@@ -1,7 +1,7 @@
 """Пример вывода полного имени через f‑строку."""
 
-first_name = "Денис"
-last_name = "Жмаев"
+first_name = input("Введите имя: ")
+last_name = input("Введите фамилию: ")
 
 print(f"Меня зовут: {last_name} {first_name}")
 
